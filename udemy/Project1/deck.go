84eb072a67cbf273bf1549a5376cbd050a1c038f
ilya0 <iosovets@gmail.com>
2018-09-06 16:52:58 -0700

    working

--- udemy/Project1/deck.go
+++ udemy/Project1/deck.go
@@ -1,5 +1,25 @@
 package main
 
-type deck []strings{}
+import "fmt"
 
-// this deck type is a string arraysdf sf
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts"}
+	cardValues := []string{"aces", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of  "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- udemy/Project1/main.go
+++ udemy/Project1/main.go
@@ -1,19 +1,6 @@
 package main
 
-import "fmt"
-
-func main(){
-	//var card string = "Ace of Spades"
-
-	cards := {"Ace of Spades", newCard()}
-	cards := append(cards,"Six of Spades")
-
-	for i, card := range cards{
-		fmt.Println(i,card)
-	}
-}
-
-
-func newCard() string {
-	return "Five of Diamonds"
+func main() {
+	cards := newDeck()
+	cards.print()
 }
